a70ac58aa9b75430ab6566fcc38edd3e6db09e3d
Melissa Dunn <melissadunn@Melissas-MacBook-Pro.local>
2022-06-13 11:02:40 -0600

    refactor to not use a class

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import load_memory
 
 def main():
     print("*** Welcome to UVSim! ***")
-    load_memory.Memory().load()
+    load_memory.load()
     print("*** Program execution begins  ***\n")
 
     # TODO: This will be updated to the correct format in future work
-    print(load_memory.Memory().memory_dict)
+    print(load_memory.memory_dict)
 
 if __name__ == "__main__":
     main()
