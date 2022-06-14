b74cbfb4c4f2d44d9660ed75f0051d559e5af98b
Melissa Dunn <60406065+melissa-dunn@users.noreply.github.com>
2022-06-13 21:17:00 -0600

    Add greeting, memory dictionary, and capture user input (#1)
    * Add greeting, memory dictionary, and capture user input
    
    * break into modules and add validation
    
    * refactor to not use a class
    
    * add read method
    
    Co-authored-by: Melissa Dunn <melissadunn@Melissas-MacBook-Pro.local>

---
+++ main.py
@@ -0,0 +1,12 @@
+# The main file to run UVSimulator
+import memory
+
+def main():
+    print("*** Welcome to UVSim! ***")
+    memory.load()
+    print("*** Program execution begins  ***\n")
+
+    memory.read()
+
+if __name__ == "__main__":
+    main()
