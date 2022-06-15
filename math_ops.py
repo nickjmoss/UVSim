c29af9fd9cf146b432d145db72622b756f7ad6ea
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2022-06-15 11:33:18 -0600

    added the add function

--- math_ops.py
+++ math_ops.py
@@ -1,11 +1,25 @@
-def add(val1, val2):
-    return val1 + val2
+import registers as reg
+import memory
 
-def multiply(val1, val2):
-	return val1 * val2
+def convert(num):
+	string_value = str(num)
+	if string_value[0] == '1':
+		return -(int(string_value[1:]))
+	else:
+		return int(string_value[1:])
 
-def divide(val1, val2):
-	return val1 / val2
+def add(location):
+	val1 = convert(reg.registers['ACC'])
+	val2 = convert(memory.memory_dict[location])
+	result = val1 + val2
+	if result > 9999 or result < -9999:
+		return False
 
-def subtract(val1, val2):
-	return val1 - val2
+	if result < 0:
+		result = '1' + str(result)[1:]
+	else:
+		result = '0' + str(result)
+
+	reg.registers['ACC'] = result
+
+	return True
