fed220dfef0b58b3f0a89a832824dba044ecb2b3
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2022-06-15 11:32:26 -0600

    updated registers to hold strings

--- registers.py
+++ registers.py
@@ -1,4 +1,4 @@
 # registers is a dictionary object that can be called as a global object
 
-registers = {"00": 00000, "01": 00000, "02": 00000, "03": 00000, "04": 00000,
-"PC": 00000, "ACC": 00000}
+registers = {"00": "00000", "01": "00000", "02": "00000", "03": "00000", "04": "00000",
+"PC": "00000", "ACC": "00000"}
