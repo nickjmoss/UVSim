793b21d197504c3e0d850b310c80a636900c7879
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2022-06-15 10:36:46 -0600

    updates

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 # The main file to run UVSimulator
 import memory
+import math_ops
+
 
 def main():
     print("*** Welcome to UVSim! ***")
     memory.load()
     print("*** Program execution begins  ***\n")
-    
+
     mem_dict = memory.memory_dict
-    
-    #op_codes is list with all opperand codes with "0" at beggining indecated for positive num
-    op_codes = ["010", "011", "020", "021", "030", "031", "032", "033", "040", "041", "042", "043"]
+
+    # op_codes is list with all opperand codes with "0" at beggining indecated for positive num
+    op_codes = [
+        "010",
+        "011",
+        "020",
+        "021",
+        "030",
+        "031",
+        "032",
+        "033",
+        "040",
+        "041",
+        "042",
+        "043",
+    ]
 
     for word in mem_dict.keys():
 
@@ -17,56 +32,57 @@ def main():
 
         if op_code in op_codes:
 
-            #fuctions will be called in order corrisponding to their identifiers
+            # fuctions will be called in order corrisponding to their identifiers
 
             if op_code == "010":
-                #TODO: import Read function here
+                # TODO: import Read function here
                 pass
-            
+
             if op_code == "011":
-                #TODO: import Write function here
+                # TODO: import Write function here
                 pass
 
             if op_code == "020":
-                #TODO: import Load function
+                # TODO: import Load function
                 pass
 
             if op_code == "021":
-                #TODO: import Store function
+                # TODO: import Store function
                 pass
 
             if op_code == "030":
-                #TODO: import Add function
+                # TODO: import Add function
                 pass
 
             if op_code == "031":
-                #TODO: import Subtract function
+                # TODO: import Subtract function
                 pass
 
             if op_code == "032":
-                #TODO: import Mulitiply function
+                # TODO: import Mulitiply function
                 pass
-            
+
             if op_code == "033":
-                #TODO: import Devide function
+                # TODO: import Devide function
                 pass
 
             if op_code == "040":
-                #TODO: import Branch function
+                # TODO: import Branch function
                 pass
 
             if op_code == "041":
-                #TODO: import BranchNeg function
+                # TODO: import BranchNeg function
                 pass
 
             if op_code == "042":
-                #TODO: import BranchZero function
+                # TODO: import BranchZero function
                 pass
 
             if op_code == "043":
-                #TODO: import Halt function
+                # TODO: import Halt function
                 pass
     memory.read()
 
+
 if __name__ == "__main__":
-	main()
+    main()
