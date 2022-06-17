227874da6aa658337e3a1198cd126f277ed304fc
GavinDoel <doel.gavin@gmail.com>
2022-06-17 15:29:52 -0600

    Changes made for BranchNEG and BranchZERO

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # The main file to run UVSimulator
 import memory
 import branch
+import registers
 
 def main():
     print("*** Welcome to UVSim! ***")
@@ -12,8 +13,18 @@ def main():
     #op_codes is list with all opperand codes with "0" at beggining indecated for positive num
     op_codes = ["010", "011", "020", "021", "030", "031", "032", "033", "040", "041", "042", "043"]
 
-    for word in mem_dict.keys():
+    #iteration count is used for branching
+    iter_count = 0
 
+    while iter_count != 100:
+        
+        #If iteration less than ten, convert to string, and add zero at beginning
+        if iter_count < 10:
+            word = "0"+str(iter_count)
+        #Else: just convert to string
+        else:
+            word = str(iter_count)
+        
         op_code = mem_dict[word][:3]
 
         if op_code in op_codes:
@@ -54,21 +65,30 @@ def main():
 
             if op_code == "040":
                 branch.branch(mem_dict[word][3:])
+                iter_count = int(mem_dict[word][3:])
                 continue      
 
             if op_code == "041":
-                #TODO: import BranchNeg function
-                pass
+                if int(registers.registers["ACC"]) < 0:
+                    branch.branch(mem_dict[word][3:])
+                    iter_count = int(mem_dict[word][3:])
+                    continue
 
             if op_code == "042":
-                #TODO: import BranchZero function
-                pass
+                if int(registers.registers["ACC"]) == 0:
+                    branch.branch(mem_dict[word][3:])
+                    iter_count = int(mem_dict[word][3:])
+                    continue
 
             if op_code == "043":
                 #TODO: import Halt function
                 pass
+ 
+        #Update Iteration count and assign PC
 
-            # TODO: PC will be updated to next memory location
+        iter_count += 1
+        registers.registers["PC"] = str(iter_count)
+
     memory.read()
 
 if __name__ == "__main__":
