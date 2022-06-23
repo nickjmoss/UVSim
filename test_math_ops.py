66bfca9ff14dcfa29723f2d2ac1709d97d77196a
kyleMeiners <103973355+kyleMeiners@users.noreply.github.com>
2022-06-23 12:41:48 -0600

    Update test_math_ops.py
    Added function docstrings

--- test_math_ops.py
+++ test_math_ops.py
@@ -3,19 +3,23 @@ import registers
 import memory
 
 def test_num_format():
+    '''Tests that integers are being converted to strings appropriately'''
     assert math_ops.num_format("3") == "0003"
     print("test_num_format - pass")
 
 def test_final_result():
+    '''Tests that sign bits are being added correctly'''
     assert math_ops.final_result(-5) == "10005"
     assert math_ops.final_result(5) == "00005"
     print("test_final_result - pass")
 
 def test_convert():
+    '''Tests that strings are converted to integers correctly'''
     assert math_ops.convert("10005") == -5
     assert math_ops.convert("00005") == 5
 
 def test_add():
+    '''Tests for correct addition with positive and negative numbers'''
     registers.registers["ACC"] = "00003"
     memory.memory_dict["05"] = "00007"
     math_ops.add("05")
@@ -27,6 +31,7 @@ def test_add():
     print("test_add - pass")
 
 def test_subtract():
+    '''Tests for correct subtraction with positive and negative numbers'''
     registers.registers["ACC"] = "00009"
     memory.memory_dict["05"] = "00002"
     math_ops.subtract("05")
@@ -40,6 +45,7 @@ def test_subtract():
 
 
 def test_multiply():
+    '''Tests for correct multiplication with positive and negative numbers'''
     registers.registers["ACC"] = "00005"
     memory.memory_dict["05"] = "00002"
     math_ops.multiply("05")
@@ -52,6 +58,7 @@ def test_multiply():
     print("test_multiply - pass")
 
 def test_divide():
+    '''Tests for correct division with positive and negative numbers'''
     registers.registers["ACC"] = "00020"
     memory.memory_dict["05"] = "00005"
     math_ops.divide("05")
@@ -77,8 +84,3 @@ test_divide()
 
 
 print("All test cases passed")
-
-
-#notes for math_ops.py
-#line 9, camelcase is used instead of snake case, inconsistent
-#maybe a warning if the end result is too high or too low? and divide by zero
