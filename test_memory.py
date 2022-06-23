7d64b0d89661fc63a92efaf41d168fae9f7d8897
kyleMeiners <103973355+kyleMeiners@users.noreply.github.com>
2022-06-23 12:43:23 -0600

    Update test_memory.py
    Added docstrings

--- test_memory.py
+++ test_memory.py
@@ -5,6 +5,7 @@ import memory
 
 
 def test_memory_created():
+    '''Tests that a data structure is created to hold the necessary amount of data points'''
     assert isinstance(memory.memory_dict, dict)
     assert len(memory.memory_dict) >= 100
 
@@ -13,6 +14,7 @@ def test_memory_created():
 
 
 def test_load():
+    '''Tests that data can be loaded from memory'''
     assert memory.memory_dict["00"] == "00000"
     assert memory.memory_dict["01"] == "00000"
     with mock.patch('builtins.input', return_value="+1004"):
