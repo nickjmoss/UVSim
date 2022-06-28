17910e8ffb91a0414002dd40126e8eb8285f3283
kyleMeiners <103973355+kyleMeiners@users.noreply.github.com>
2022-06-28 12:06:02 -0600

    Converted to OOP

--- math_ops.py
+++ math_ops.py
@@ -4,70 +4,73 @@ Author: Nick Moss
 import registers
 import memory
 
-def num_format(num):
-	while len(num) < 4:
-		num = '0' + num
-	return num
+class Math_ops:
 
-def final_result(num):
-	if num < 0:
-		formatted_num = num_format(str(num)[1:])
-		num = '1' + formatted_num
-	else:
-		formatted_num = num_format(str(num))
-		num = '0' + formatted_num
-	
-	return num
 
-def convert(num):
-	string_value = str(num)
-	if string_value[0] == '1':
-		return -(int(string_value[1:]))
-	else:
-		return int(string_value[1:])
+	def num_format(self, num):
+		while len(num) < 4:
+			num = '0' + num
+		return num
 
-def add(location):
-	val1 = convert(registers.registers['ACC'])
-	val2 = convert(memory.memory_dict[location])
-	result = val1 + val2
-	if result > 9999 or result < -9999:
-		return False
+	def final_result(self, num):
+		if num < 0:
+			formatted_num = num_format(str(num)[1:])
+			num = '1' + formatted_num
+		else:
+			formatted_num = num_format(str(num))
+			num = '0' + formatted_num
 
-	registers.registers['ACC'] = final_result(result)
+		return num
 
-	return True
+	def convert(self, num):
+		string_value = str(num)
+		if string_value[0] == '1':
+			return -(int(string_value[1:]))
+		else:
+			return int(string_value[1:])
 
-def subtract(location):
-	val1 = convert(registers.registers['ACC'])
-	val2 = convert(memory.memory_dict[location])
-	result = val1 - val2
-	if result > 9999 or result < -9999:
-		return False
+	def add(self, location):
+		val1 = convert(registers.registers['ACC'])
+		val2 = convert(memory.memory_dict[location])
+		result = val1 + val2
+		if result > 9999 or result < -9999:
+			return False
 
-	registers.registers['ACC'] = final_result(result)
+		registers.registers['ACC'] = final_result(result)
 
-	return True
+		return True
 
-def multiply(location):
-	val1 = convert(registers.registers['ACC'])
-	val2 = convert(memory.memory_dict[location])
-	result = val1 * val2
-	if result > 9999 or result < -9999:
-		return False
+	def subtract(self, location):
+		val1 = convert(registers.registers['ACC'])
+		val2 = convert(memory.memory_dict[location])
+		result = val1 - val2
+		if result > 9999 or result < -9999:
+			return False
 
-	registers.registers['ACC'] = final_result(result)
+		registers.registers['ACC'] = final_result(result)
 
-	return True
+		return True
 
-def divide(location):
-	val1 = convert(registers.registers['ACC'])
-	val2 = convert(memory.memory_dict[location])
-	if val2 == 0:
-		return False
-	result = val1 // val2
-	if result > 9999 or result < -9999:
-		return False
+	def multiply(self, location):
+		val1 = convert(registers.registers['ACC'])
+		val2 = convert(memory.memory_dict[location])
+		result = val1 * val2
+		if result > 9999 or result < -9999:
+			return False
 
-	registers.registers['ACC'] = final_result(result)
+		registers.registers['ACC'] = final_result(result)
 
-	return True
+		return True
+
+	def divide(self, location):
+		val1 = convert(registers.registers['ACC'])
+		val2 = convert(memory.memory_dict[location])
+		if val2 == 0:
+			return False
+		result = val1 // val2
+		if result > 9999 or result < -9999:
+			return False
+
+		registers.registers['ACC'] = final_result(result)
+
+		return True
