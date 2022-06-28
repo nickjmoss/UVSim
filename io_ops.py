4109b84f4b2bcba765fad44354005f2fb081bc95
kyleMeiners <103973355+kyleMeiners@users.noreply.github.com>
2022-06-28 12:09:01 -0600

    io_ops.py
    converted to OOP

--- io_ops.py
+++ io_ops.py
@@ -5,23 +5,25 @@ Author: Melissa Dunn
 import re
 import memory
 
-def read(location):
-    '''Read a word from the keyboard into a specific location in memor'''
+class Io_ops:
+    
+    def read(self, location):
+        '''Read a word from the keyboard into a specific location in memor'''
 
-    while True:
-        print("Enter an integer: ", end="")
+        while True:
+            print("Enter an integer: ", end="")
 
-        user_input = input()
+            user_input = input()
 
-        if(re.fullmatch("^[+-]?\d{1,4}", user_input) is None):
-            print("Invalid input. Try again.")
-        else:
-            break
-    user_input = user_input.replace("+", "0").replace("-", "1")
-    memory.memory_dict[location] = (user_input).zfill(5)
+            if(re.fullmatch("^[+-]?\d{1,4}", user_input) is None):
+                print("Invalid input. Try again.")
+            else:
+                break
+        user_input = user_input.replace("+", "0").replace("-", "1")
+        memory.memory_dict[location] = (user_input).zfill(5)
 
-def write(location):
-    '''Write a word from a specific location in memory to screen.'''
+    def write(self, location):
+        '''Write a word from a specific location in memory to screen.'''
 
-    output = re.sub(r'^1', "-", memory.memory_dict[location])
-    print(f"Contents of {int(location)} is {int(output)}")
+        output = re.sub(r'^1', "-", memory.memory_dict[location])
+        print(f"Contents of {int(location)} is {int(output)}")
