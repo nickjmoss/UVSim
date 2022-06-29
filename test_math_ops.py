9b1054689add0b24676411dab04c43e5b4c7dab9
kyleMeiners <103973355+kyleMeiners@users.noreply.github.com>
2022-06-28 20:57:32 -0600

    Update test_math_ops.py

--- test_math_ops.py
+++ test_math_ops.py
@@ -1,6 +1,6 @@
 '''Author: Kyle Meiners'''
 
-from math_ops import Math_ops
+from math_ops import Math_Ops
 import registers
 import memory as mem
 
@@ -8,77 +8,77 @@ memory = mem.Memory()
 
 def test_num_format():
     '''Tests that integers are being converted to strings appropriately'''
-    math = Math_ops()
+    math = Math_Ops()
     assert math.num_format("3") == "0003"
     print("test_num_format - pass")
 
 def test_final_result():
     '''Tests that sign bits are being added correctly'''
-    math = Math_ops()
+    math = Math_Ops()
     assert math.final_result(-5) == "10005"
     assert math.final_result(5) == "00005"
     print("test_final_result - pass")
 
 def test_convert():
     '''Tests that strings are converted to integers correctly'''
-    math = Math_ops()
+    math = Math_Ops()
     assert math.convert("10005") == -5
     assert math.convert("00005") == 5
 
 def test_add():
     '''Tests for correct addition with positive and negative numbers'''
-    math = Math_ops()
+    math = Math_Ops()
     registers.registers["ACC"] = "00003"
     memory.memory_dict["05"] = "00007"
-    math.add("05", memory)
+    math.add("05")
     assert registers.registers["ACC"] == "00010"
 
     memory.memory_dict["05"] = "10002"
-    math.add("05", memory)
+    math.add("05")
     assert registers.registers["ACC"] == "00008"
     print("test_add - pass")
 
 def test_subtract():
     '''Tests for correct subtraction with positive and negative numbers'''
-    math = Math_ops()
+    math = Math_Ops()
     registers.registers["ACC"] = "00009"
     memory.memory_dict["05"] = "00002"
-    math.subtract("05", memory)
+    math.subtract("05")
     assert registers.registers["ACC"] == "00007"
 
     registers.registers["ACC"] = "00009"
     memory.memory_dict["05"] = "10002"
-    math.subtract("05", memory)
+    math.subtract("05")
     assert registers.registers["ACC"] == "00011"
     print("test_subtract - pass")
 
 
 def test_multiply():
     '''Tests for correct multiplication with positive and negative numbers'''
-    math = Math_ops()
+    math = Math_Ops()
     registers.registers["ACC"] = "00005"
     memory.memory_dict["05"] = "00002"
-    math.multiply("05", memory)
+    math.multiply("05")
     assert registers.registers["ACC"] == "00010"
 
     registers.registers["ACC"] = "10007"
     memory.memory_dict["05"] = "00003"
-    math.multiply("05", memory)
+    math.multiply("05")
     assert registers.registers["ACC"] == "10021"
     print("test_multiply - pass")
 
 def test_divide():
     '''Tests for correct division with positive and negative numbers'''
-    math = Math_ops()
+    math = Math_Ops()
     registers.registers["ACC"] = "00020"
     memory.memory_dict["05"] = "00005"
-    math.divide("05", memory)
+    math.divide("05")
     assert registers.registers["ACC"] == "00004"
     print("test_divide - pass")
 
     registers.registers["ACC"] = "10015"
     memory.memory_dict["05"] = "00005"
-    math.divide("05", memory)
+    math.divide("05")
     assert registers.registers["ACC"] == "10003"
     print("test_multiply - pass")
 
