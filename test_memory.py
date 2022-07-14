78f45ea1d66702fed448846b5d90c69f81ba0ea7
Nick J Moss <78372873+nickjmoss@users.noreply.github.com>
2022-07-14 16:55:16 -0600

    Update test_memory.py

--- test_memory.py
+++ test_memory.py
@@ -23,10 +23,18 @@ def test_init():
     print("test_load - pass")
 
 
+def test_reset():
+	memory.memory_dict["00"] = "11111"
+	memory.reset();
+	assert memory.memory_dict["00"] == "00000"
+
+	print("Memory reset test passed")
+
 
 
 test_memory_created()
 test_init()
+test_reset()
 
 print("All test cases passed")
 
