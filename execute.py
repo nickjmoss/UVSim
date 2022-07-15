1e72cdb10ee07a995feddbf4d15ac968bbffc4a7
Melissa Dunn <60406065+melissa-dunn@users.noreply.github.com>
2022-07-14 18:39:45 -0600

    Gui and console (#29)
    * wip
    
    * clean up display
    
    * wip
    
    * reset memory before re-running program
    
    * set background and foreground for text boxes
    
    * fixup
    
    * remove unused vars
    
    * add gui and console
    
    * add main.py
    
    Co-authored-by: Melissa Dunn <melissadunn@Melissas-MacBook-Pro.local>

---
+++ execute.py
@@ -0,0 +1,108 @@
+# The main file to run UVSimulator
+import math_ops
+import branch
+import load
+import store
+import registers
+import io_ops
+
+def execute(memory):
+	math = math_ops.Math_Ops()
+	io = io_ops.IO()
+	br = branch.Branch()
+
+	mem_dict = memory.memory_dict
+
+	# op_codes is list with all opperand codes with "0" at beggining indecated for positive num
+	op_codes = ["010","011","020","021","030","031","032","033","040","041","042","043"]
+
+	#iteration count is used for branching
+	iter_count = 0
+
+	while iter_count != 100:
+
+		#If iteration less than ten, convert to string, and add zero at beginning
+		if iter_count < 10:
+			word = "0"+str(iter_count)
+		#Else: just convert to string
+		else:
+			word = str(iter_count)
+
+		op_code = mem_dict[word][:3]
+		location = mem_dict[word][-2:]
+
+		if op_code in op_codes:
+			# fuctions will be called in order corrisponding to their identifiers
+
+			if op_code == "010":
+				'''Author: Melissa Dunn'''
+				io.read_gui(location, memory)
+
+			if op_code == "011":
+				'''Author: Melissa Dunn'''
+				io.write_gui(location, memory)
+
+			if op_code == "020":
+				'''Author: Nick Moss'''
+				load.load(location, memory)
+
+			if op_code == "021":
+				'''Author Gavin Doel'''
+				store.store(location, memory)
+
+			if op_code == "030":
+				'''Author: Nick Moss'''
+				result = math.add(location, memory)
+				if result is False:
+					print("The sum of the values was too big or too small to handle")
+					break
+
+			if op_code == "031":
+				'''Author: Nick Moss'''
+				result = math.subtract(location, memory)
+				if result is False:
+					print("The difference of the values was too big or too small to handle")
+					break
+
+			if op_code == "032":
+				'''Author: Nick Moss'''
+				result = math.multiply(location, memory)
+				if result is False:
+					print("The product of the values was too big or too small to handle")
+					break
+
+			if op_code == "033":
+				'''Author: Nick Moss'''
+				result = math.divide(location, memory)
+				if result is False:
+					print("The difference of the values was too big or too small to handle, or there was a division by zero")
+					break
+
+			if op_code == "040":
+				'''Author: Gavin Doel'''
+				br.branch(location)
+				iter_count = int(location)
+				continue
+
+			if op_code == "041":
+				'''Author: Gavin Doel'''
+				if int(registers.registers["ACC"]) < 0:
+					br.branch(location)
+					iter_count = int(location)
+					continue
+
+
+			if op_code == "042":
+				'''Author: Gavin Doel'''
+				if int(registers.registers["ACC"]) == 0:
+					br.branch(location)
+					iter_count = int(location)
+					continue
+
+
+			if op_code == "043":
+				break
+
+		#Update Iteration count and assign PC
+		iter_count += 1
+		registers.registers["PC"] = str(iter_count)
--- memory.py
+++ memory.py
@@ -3,10 +3,14 @@ Author: Melissa Dunn
 '''
 
 import re
+from tkinter import *
 
 class Memory:
 
     memory_dict = {str(x).zfill(2): "00000" for x in range(0,100)}
+
+    def reset(self):
+        Memory.memory_dict = {str(x).zfill(2): "00000" for x in range(0,100)}
 
     def read(self):
         cols = [str(x).zfill(2) for x in range(0,10)]
@@ -23,6 +27,34 @@ class Memory:
                 position = str(int(row) + int(col)).zfill(2)
                 print(Memory.memory_dict[position], end=" ")
             print()
+
+    def read_gui(self, text_widget):
+        def print_on_gui(*args, sep=" ", end="\n"):
+            text = sep.join(args) + end
+            # Set the Text widget's state to normal so that we can edit its text
+            text_widget.config(state="normal")
+            # Insert the text at the end
+            text_widget.insert("end", text)
+            # Set the Text widget's state to disabled to disallow the user changing the text
+            text_widget.config(state="disabled")
+
+        # Show the widget on the screen
+        text_widget.pack(fill="both", expand=True)
+
+        cols = [str(x).zfill(2) for x in range(0,10)]
+        rows = [str(x).zfill(2) for x in range(0,100,10)]
+
+        print_on_gui("   ", end="")
+        for col in cols:
+            print_on_gui("{:>6}".format(col), end="")
+        print_on_gui()
+
+        for row in rows:
+            print_on_gui(row, end="  ")
+            for col in cols:
+                position = str(int(row) + int(col)).zfill(2)
+                print_on_gui(Memory.memory_dict[position], end=" ")
+            print_on_gui()
 
     def init(self):
         print(
@@ -55,3 +87,20 @@ class Memory:
             # Parse user input for memory ( + => 0 and - => 1 )
             user_input = user_input.replace("+", "0").replace("-", "1")
             Memory.memory_dict[location] = user_input
+
+    def load_gui(self, program):
+        program = program.split("\n")
+        try:
+            for location, instruction in zip(Memory.memory_dict, program):
+                if(instruction == "-99999"):
+                    Label(fg="#0A0", text="*** Program loading completed ***").pack()
+                    raise StopIteration
+
+                if(re.fullmatch("^[+-]\d{4}", instruction) is None):
+                    return False
+
+                # Parse user input for memory ( + => 0 and - => 1 )
+                instruction = instruction.replace("+", "0").replace("-", "1")
+                Memory.memory_dict[location] = instruction
+        except StopIteration:
+            pass
